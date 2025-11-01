23744c23bf2fee2dd08cce82e05c0dc748f9f67b
wandouuu <130308899+wandouuu@users.noreply.github.com>
2025-11-01 17:38:04 -0400

    Add main.ino via upload

---
+++ esp32/main/main.ino
@@ -0,0 +1,12 @@
+#include "sensors.h"
+#include "BluetoothSerial.h"
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
