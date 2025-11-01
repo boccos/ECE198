015ad314cc63a4f14b55af679db430ab6280cac8
wandouuu <130308899+wandouuu@users.noreply.github.com>
2025-11-01 17:31:54 -0400

    Modify SPO2 and HR datatypes

---
+++ esp32/main/sensors.h
@@ -0,0 +1,32 @@
+#ifndef SENSORS_H
+#define SENSORS_H
+
+#include <Wire.h> // I2C
+#include <DFRobot_MAX30102.h> // pulse oximeter
+
+
+// Structure containing all data required
+struct sensor_data{
+    
+    // Pulse oximeter
+    int32_t spO2{};
+    int32_t heart_rate{};
+    double IR{};
+
+
+    // Accelerometer
+    double accel_x{};
+    double accel_y{};
+    double accel_z{};
+    
+    // Switches
+    double response_time{}; // response time to button press
+    bool answered_correctly{}; // if patient answered the question correctly
+};
+
+// Initialize sensors
+void setup_sensors();
+sensor_data retrieve_data();
+
+
+#endif
