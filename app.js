afacae69ad7e40226b6c8bbd56c215a06b3b337d
boccos <145885073+boccos@users.noreply.github.com>
2025-11-06 17:42:29 -0500

    app.js reads JSON and writes as js

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 console.log("Server Up");
+
+const { log } = require('console');
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs').promises;
+
 
 const app = express();
 const PORT = 3000;
@@ -17,3 +20,20 @@ app.use((req, res) => {
 app.listen(PORT, () => {
     console.log("Listening to Server");
 });
+
+async function sendData(){
+    try{
+        const inputPath = path.resolve(__dirname, 'sensors', 'sensorData.json');
+        const outputPath = path.resolve(__dirname, 'data', 'data.txt');
+
+        const rawData = await fs.readFile(inputPath, 'utf-8');
+        const data = JSON.parse(rawData);
+        await fs.writeFile(outputPath, JSON.stringify(data) + '\n', { flag: 'a', encoding: 'utf-8' });
+        console.log("Data Copied Success!");
+    }
+    catch(err){
+        console.log(`Data Failed to Copy ${err.message}`);
+    }
+}
+
+// setInterval(sendData, 10000)
