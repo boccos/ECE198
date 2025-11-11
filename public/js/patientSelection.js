7c6774c58c42ca5e0a7742aced75cc89ad8ba5c1
rootrc <rootrc123@gmail.com>
2025-11-11 17:25:03 -0500

    added to patient selection form

---
+++ public/js/patientSelection.js
@@ -0,0 +1,35 @@
+const patients = [
+    new Patient(0, "John", "Doe"),
+    new Patient(1, "Jane", "Smith"),
+];
+
+const select = document.getElementById('patientSelect');
+const infoDiv = document.getElementById('patientInfo');
+const addOption = select.querySelector('option[value="add"]');
+
+patients.forEach((patient) => {
+    const option = document.createElement("option");
+    option.value = patient.id;
+    option.textContent = patient.getFullName();
+    select.insertBefore(option, addOption);
+});
+
+function showPatientInfo() {
+    const selectedId = select.value;
+    const patient = patients[selectedId];
+    infoDiv.innerHTML = `
+        <h3>${patient.getFullName()}'s information is currently being displayed!</h3>
+      `;
+}
+
+function openPatientForm() {
+    
+}
+
+select.addEventListener('change', (event) => {
+    if (event.target.value === 'add') {
+        openPatientForm(); 
+    } else {
+        showPatientInfo();
+    }
+});
