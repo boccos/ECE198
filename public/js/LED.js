8218601e0b28085dfc2aff94b6808d57e455b134
rootrc <rootrc123@gmail.com>
2025-11-21 14:44:08 -0500

    finished light control buttons

--- public/js/LED.js
+++ public/js/LED.js
@@ -1,12 +1,28 @@
-import {leftOn, rightOn} from './fetch/fetchLed.js'
+import { leftOn, rightOn } from './fetch/fetchLed.js'
 
 const leftButton = document.getElementById("left-LED-button");
 const rightButton = document.getElementById("right-LED-button");
+const resetButton = document.getElementById("reset-button");
+
+disableButtons(false);
 
 leftButton.addEventListener("click", async function () {
     leftOn();
+    disableButtons(true);
 });
 
 rightButton.addEventListener("click", async function () {
     rightOn();
+    disableButtons(true);
 });
+
+resetButton.addEventListener("click", async function () {
+    disableButtons(false);
+});
+
+function disableButtons(flag) {
+    leftButton.disabled = flag;
+    rightButton.disabled = flag;
+    resetButton.disabled = !flag;
+}
+
